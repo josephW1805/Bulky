befa3a8f60b07a2bb67bb4e0770731d68ba7e103
josephW1805 <joseph.weng.1805@gmail.com>
2023-05-24 21:05:46 -0400

    Product Model with CRUD

---
+++ BulkyWeb/wwwroot/js/product.js
@@ -0,0 +1,68 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+})
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/admin/product/getall"
+        },
+        "columns": [
+            { "data": "title", "width": "25%" },
+            { "data": "isbn", "width": "15%" },
+            { "data": "listPrice", "width": "10%" },
+            { "data": "author", "width": "15%" },
+            { "data": "category.name", "width": "10%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                        <div class="w-75 btn-group" role="group">
+                            <a href="/Admin/Product/Upsert?id=${data}" class="btn btn-primary mx-2">
+                                <i class="bi bi-pencil-square"></i>
+                            </a>
+                            <a onClick=Delete('/Admin/Product/Delete/${data}') class="btn btn-danger">
+                                <i class="bi bi-trash"></i>
+                            </a>
+                        </div>
+                    `
+                },
+                "width": "25%"
+            },
+        ],
+        'columnDefs': [{
+            'targets': [5],
+            'orderable': false,
+        }]
+    });
+}
+
+function Delete(url) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#198754',
+        cancelButtonColor: '#DC3545',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (data) {
+                    if (data.success) {
+                        dataTable.ajax.reload();
+                        toastr.success(data.message);
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            })
+        }
+    })
+}
